59af05409b47736084d7444eb6ee9c1301c3daf2
IanKu304 <115488463+IanKu304@users.noreply.github.com>
2022-12-28 14:36:28 +0800

    Add files via upload

---
+++ 1227_RelativeWords/Project-main/src/final1/Sort.java
@@ -0,0 +1,68 @@
+package final1;
+
+import java.util.ArrayList;
+
+
+public class Sort {
+	private ArrayList<TreeRootList> lst;
+	
+	public Sort(){
+		this.lst = new ArrayList<TreeRootList>();
+    }
+	
+	public void add(TreeRootList keyword){
+		lst.add(keyword);
+    }
+	
+	//Quick sort
+	public void sort(){
+		if(lst.size() == 0)
+		{
+			System.out.println("InvalidOperation");
+		}
+		else 
+		{
+			quickSort(0, lst.size()-1);
+		}
+	}
+	
+	private void quickSort(int leftbound, int rightbound){
+		//1. Implement QuickSort algorithm
+		 if(leftbound < rightbound) {
+	            int i = leftbound; // 由左至右的索引
+	            int j = rightbound + 1; // 由右至左的索引
+	            while(true) {
+	                while( i+1 < lst.size() && lst.get(i++).count < lst.get(leftbound).count); // 向右找, 直到找到比基準值大的數
+	                while( j-1 >= 0 && lst.get(--j).count> lst.get(leftbound).count); // 向左找, 值到找到比基準值小的數
+	                if( i >= j) break; // 若i,j重疊或i超過j後則退出循環
+	                swap(i , j);
+	            }
+	            swap(leftbound , j); // 基準點與 j 交換
+	            quickSort(leftbound , j - 1); // 遞迴排序基準點左子序列
+	            quickSort(j + 1 , rightbound); // 遞迴排序基準點右子序列
+	            
+	        }
+		
+
+	}
+	
+	private void swap(int aIndex, int bIndex){
+		TreeRootList temp = lst.get(aIndex);
+		lst.set(aIndex, lst.get(bIndex));
+		lst.set(bIndex, temp);
+	}
+	
+	public void output(){
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = lst.size()-1; i > 0; i--){
+			if(lst.get(i).count>0.001)
+			System.out.println(lst.get(i).name+" "+lst.get(i).count);
+			/*TreeRootList k = lst.get(i);
+			if(i > 0)sb.append(" ");
+			sb.append(k.toString());*/
+		}
+		
+		//System.out.println(sb.toString());	
+	}
+}
---
+++ 1227_RelativeWords/Project-main/src/final1/suburl.java
@@ -0,0 +1,66 @@
+package final1;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class suburl {
+    String htmlUrl;
+    ArrayList<String> hrefList = new ArrayList();
+    String charSet;
+
+    public suburl(String htmlUrl) {
+        // TODO 自動生成的建構函式存根
+        this.htmlUrl = htmlUrl;
+    }
+
+    public ArrayList<String> parser() throws IOException {  //獲得該網頁下的超連結
+    	try {
+        URL url = new URL(htmlUrl);           //建立URL物件，建立連線
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setDoOutput(true);
+        String contenttype = connection.getContentType();
+        charSet = getCharset(contenttype);
+        System.out.println(contenttype);
+        InputStreamReader isr = new InputStreamReader(connection.getInputStream(), "gb2312");   //建立輸入流
+        BufferedReader br = new BufferedReader(isr);
+        String str = null, rs = null;
+        
+        while ((str = br.readLine()) != null) {
+        	System.out.println(str);
+            Pattern pattern = Pattern.compile("<a href=(.*?)>");    //識別這一行是否符合網頁的格式
+            Matcher matcher = pattern.matcher(str);
+
+            while (matcher.find()) {
+                Pattern pattern1 = Pattern.compile("\"(.*?)\"");
+                Matcher matcher1 = pattern1.matcher(matcher.group(1));
+                if (matcher1.find()) {
+                    rs = matcher1.group(1);      //將本行引號中的內容截取出來
+                }
+                if (rs.indexOf("https") != -1) {  //帶http的為URL
+                    if (rs != null && hrefList.size() < 20 ) {
+                    	hrefList.add(rs);
+                    }
+                }
+            }
+        }
+        }catch(Exception e) {}
+        return hrefList;
+    }
+
+    public void getURL() throws IOException {       //獲得每個超連結對應的web網頁
+        ArrayList<String> URLList = parser();
+    }
+    
+    private String getCharset(String str) {  //獲取網頁編碼方式，有些網頁沒有提供，所以暫時不使用
+        Pattern pattern = Pattern.compile("charset=.*");
+        Matcher matcher = pattern.matcher(str);
+        if (matcher.find())
+            return matcher.group(0).split("charset=")[1];
+        return null;
+    }
+
+}
