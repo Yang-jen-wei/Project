b1fbf640f431e781fe99fd110bcb862e380c737d
Josie <joshyu891221@gmail.com>
2023-01-04 10:41:38 +0800

    建立server 並新增web介面

---
+++ Web/src/TreeRootList.java
@@ -0,0 +1,16 @@
+
+
+public class TreeRootList {
+		public String name;
+	    public double count;
+	    
+	    public TreeRootList(String name, double count){
+			this.name = name;
+			this.count = count;
+	    }
+	    
+	    @Override
+	    public String toString(){
+	    	return "["+name+","+count+"]";
+	    }
+	}
